d40c536020a09df79392d92552f4870c56320882
hivislav <hivislav@gmail.com>
2022-09-14 17:39:46 +0500

    create domain

---
+++ app/src/main/java/ru/hivislav/shoppinglistapp/domain/GetShopListUseCase.kt
@@ -0,0 +1,8 @@
+package ru.hivislav.shoppinglistapp.domain
+
+class GetShopListUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun getShopList(): List<ShopItem> {
+        return shopListRepository.getShopList()
+    }
+}
