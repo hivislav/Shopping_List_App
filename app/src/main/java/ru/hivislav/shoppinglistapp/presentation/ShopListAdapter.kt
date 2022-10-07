c7217ab3a1156a977305b70d39a2a2ef3435668a
hivislav <hivislav@gmail.com>
2022-10-07 19:31:15 +0600

    task 3.4
    // add diff utils
    // fix problem with wrong position in adapter for try to delete items

--- app/src/main/java/ru/hivislav/shoppinglistapp/presentation/ShopListAdapter.kt
+++ app/src/main/java/ru/hivislav/shoppinglistapp/presentation/ShopListAdapter.kt
@@ -3,6 +3,7 @@ package ru.hivislav.shoppinglistapp.presentation
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import ru.hivislav.shoppinglistapp.databinding.ItemShopDisabledBinding
 import ru.hivislav.shoppinglistapp.databinding.ItemShopEnabledBinding
@@ -16,8 +17,10 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemBaseViewHold
 
     var shopItemList = listOf<ShopItem>()
         set(value) {
+            val callback = ShopListDiffCallback(shopItemList, value)
+            val diffResult = DiffUtil.calculateDiff(callback)
+            diffResult.dispatchUpdatesTo(this)
             field = value
-            notifyDataSetChanged()
         }
 
     var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
@@ -46,14 +49,6 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemBaseViewHold
 
     override fun onBindViewHolder(holder: ShopItemBaseViewHolder, position: Int) {
         holder.bind(shopItemList[position])
-        holder.itemView.setOnLongClickListener {
-            onShopItemLongClickListener?.invoke(shopItemList[position])
-            true
-        }
-
-        holder.itemView.setOnClickListener {
-            onShopItemClickListener?.invoke(shopItemList[position])
-        }
     }
 
     override fun getItemCount(): Int {
@@ -71,22 +66,40 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemBaseViewHold
         abstract fun bind(shopItem: ShopItem)
     }
 
-    class ShopItemEnabledViewHolder(private val binding: ItemShopEnabledBinding)
+    inner class ShopItemEnabledViewHolder(private val binding: ItemShopEnabledBinding)
         : ShopItemBaseViewHolder(binding.root) {
         override fun bind(shopItem: ShopItem) {
             itemView.apply {
                 binding.rvShopListItemName.text = shopItem.name
                 binding.rvShopListItemCount.text = shopItem.count.toString()
+
+                setOnLongClickListener {
+                    onShopItemLongClickListener?.invoke(shopItem)
+                    true
+                }
+
+                setOnClickListener {
+                    onShopItemClickListener?.invoke(shopItem)
+                }
             }
         }
     }
 
-    class ShopItemDisabledViewHolder(private val binding: ItemShopDisabledBinding)
+    inner class ShopItemDisabledViewHolder(private val binding: ItemShopDisabledBinding)
         : ShopItemBaseViewHolder(binding.root) {
         override fun bind(shopItem: ShopItem) {
             itemView.apply {
                 binding.rvShopListItemName.text = shopItem.name
                 binding.rvShopListItemCount.text = shopItem.count.toString()
+
+                setOnLongClickListener {
+                    onShopItemLongClickListener?.invoke(shopItem)
+                    true
+                }
+
+                setOnClickListener {
+                    onShopItemClickListener?.invoke(shopItem)
+                }
             }
         }
     }
