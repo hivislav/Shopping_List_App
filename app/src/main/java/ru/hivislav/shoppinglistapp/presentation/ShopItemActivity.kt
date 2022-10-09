b692b88f52e30f33508665e8379676055788bb84
hivislav <hivislav@gmail.com>
2022-10-09 18:41:46 +0600

    task 4
    // add shop item activity (details) and layout

---
+++ app/src/main/java/ru/hivislav/shoppinglistapp/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package ru.hivislav.shoppinglistapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.hivislav.shoppinglistapp.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
