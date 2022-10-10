2d35219c7a756f882d9b0b55430ddb1c3cf806ea
hivislav <hivislav@gmail.com>
2022-10-10 20:10:19 +0600

    task 4.2
    // setup intent functions for ItemShopActivity

--- app/src/main/java/ru/hivislav/shoppinglistapp/presentation/ShopItemActivity.kt
+++ app/src/main/java/ru/hivislav/shoppinglistapp/presentation/ShopItemActivity.kt
@@ -1,6 +1,9 @@
 package ru.hivislav.shoppinglistapp.presentation
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import ru.hivislav.shoppinglistapp.databinding.ActivityShopItemBinding
 
@@ -12,5 +15,25 @@ class ShopItemActivity : AppCompatActivity() {
         binding = ActivityShopItemBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    companion object {
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_ADD = "mode_add"
+
+        fun newIntentAddItem(context: Context): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun newIntentEditItem(context: Context, shopItemId: Int): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_SHOP_ITEM_ID, shopItemId)
+            return intent
+        }
     }
 }
